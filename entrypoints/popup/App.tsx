a1a617262e478f02d00d05e6985f3c68cc4fb0a7
sanchxt <tpbeast007@gmail.com>
2024-10-17 17:55:07 +0530

    initial commit

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./entrypoints/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
