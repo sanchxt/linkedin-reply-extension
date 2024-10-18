46fb6edbd2d0d9402ae5057aabae1f1b0f66e389
sanchxt <tpbeast007@gmail.com>
2024-10-19 00:15:26 +0530

    create footer component for popup window
    Created the footer component to be displayed in the popup window

---
+++ entrypoints/popup/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-end px-3 py-2">
+      <button className="text-base font-medium tracking-tighter">
+        Try it Out
+      </button>
+    </footer>
+  );
+};
+
+export default Footer;
