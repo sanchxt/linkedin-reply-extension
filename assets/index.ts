1e86ee5fd5d8a6dd5d07473a139e589b66b9efc0
sanchxt <tpbeast007@gmail.com>
2024-10-19 00:13:50 +0530

    Create index file to export all the icons
    Created index.ts file to export all the required icons from a common file

---
+++ assets/index.ts
@@ -0,0 +1,19 @@
+import editSvg from "./edit.svg";
+import generateSvg from "./generate.svg";
+import insertSvg from "./insert.svg";
+import regenerateSvg from "./regenerate.svg";
+import logoSvg from "./logo.svg";
+
+const editSvgIcon = editSvg;
+const generateSvgIcon = generateSvg;
+const insertSvgIcon = insertSvg;
+const regenerateSvgIcon = regenerateSvg;
+const logoSvgIcon = logoSvg;
+
+export {
+  editSvgIcon,
+  generateSvgIcon,
+  insertSvgIcon,
+  regenerateSvgIcon,
+  logoSvgIcon,
+};
