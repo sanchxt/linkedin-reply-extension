18aa3bef03fe5710dab0ba232e862e7248992c2e
sanchxt <tpbeast007@gmail.com>
2024-10-19 00:14:46 +0530

    create main component for the popup window
    Created Main component to be used in the popup window

---
+++ entrypoints/popup/components/Main.tsx
@@ -0,0 +1,16 @@
+const Main = () => {
+  return (
+    <main className="min-w-80 font-semibold w-full px-3 py-2">
+      <h3 className="text-lg text-center italic font-light">
+        No More Thinking of Apt Replies
+      </h3>
+      <p className="font-normal text-sm">
+        This chrome extension aims to increase your productivity and efficiency
+        by helping you come up with the most suitable messages in a single
+        click.
+      </p>
+    </main>
+  );
+};
+
+export default Main;
