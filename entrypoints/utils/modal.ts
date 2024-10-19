ce614224986d3a5c48a6539cde4135f3e413b98e
sanchxt <tpbeast007@gmail.com>
2024-10-20 03:28:56 +0530

    implement modal functionality

---
+++ entrypoints/utils/modal.ts
@@ -0,0 +1,44 @@
+import { Modal } from "../types/modal-type";
+import { modalHtml } from "./modalHtml";
+
+export function createModal(): Modal {
+  document.body.insertAdjacentHTML("beforeend", modalHtml);
+
+  const modalElement = document.getElementById(
+    "custom-modal"
+  ) as HTMLDivElement;
+  const generateBtn = document.getElementById(
+    "generate-btn"
+  ) as HTMLButtonElement;
+  const insertBtn = document.getElementById("insert-btn") as HTMLButtonElement;
+  const inputText = document.getElementById("input-text") as HTMLInputElement;
+  const messagesDiv = document.getElementById("messages") as HTMLDivElement;
+
+  function handleOutsideClick(event: MouseEvent) {
+    if (event.target === modalElement) modal.hide();
+  }
+
+  function handleEscKey(event: KeyboardEvent) {
+    if (event.key === "Escape") modal.hide();
+  }
+
+  const modal: Modal = {
+    element: modalElement,
+    generateBtn,
+    insertBtn,
+    inputText,
+    messagesDiv,
+    show: () => {
+      modalElement.style.display = "flex";
+      modalElement.addEventListener("click", handleOutsideClick);
+      document.addEventListener("keydown", handleEscKey);
+    },
+    hide: () => {
+      modalElement.style.display = "none";
+      modalElement.removeEventListener("click", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscKey);
+    },
+  };
+
+  return modal;
+}
