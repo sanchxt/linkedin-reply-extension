5748cd127ec829514eb401cbe01bda539fec5b1f
sanchxt <tpbeast007@gmail.com>
2024-10-20 03:28:05 +0530

    add link to the button

--- entrypoints/popup/components/Footer.tsx
+++ entrypoints/popup/components/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <footer className="flex justify-end px-3 py-2">
       <button className="text-base font-medium tracking-tighter">
-        Try it Out
+        <a href="https://www.linkedin.com/messaging/" target="_blank">
+          Try it Out
+        </a>
       </button>
     </footer>
   );
